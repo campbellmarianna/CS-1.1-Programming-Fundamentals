0b075b8ebed6df6cbf005d7da415ea30e11ea8fd
Marianna <mckd2018@gmail.com>
2018-10-15 16:08:44 -0700

    add logger.py code for functions

--- Herd_Immunity_Project/logger.py
+++ Herd_Immunity_Project/logger.py
@@ -53,8 +53,10 @@ class Logger(object):
     def __init__(self, file_name):
         # TODO:  Finish this initialization method.  The file_name passed should be the
         # full file name of the file that the logs will be written to.
-        self.file_name = None
+        self.file_name = file_name
 
+        # https://www.pythonforbeginners.com/cheatsheet/python-file-handling
+        # Also got help from Nolen Kovacik for this one:
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
                        basic_repro_num):
         # TODO: Finish this method.  The simulation class should use this method
@@ -67,7 +69,29 @@ class Logger(object):
         # since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+
+        # writes to text file
+        with open(self.file_name, "w") as file:
+            file.write(f"{population_size}\t {vax_percentage}\t {virus_name}\t {mortality_rate}\t {infection_rate}")
+            file.write("/n========================================================/n")
+            file.write(f"     Population Size: {population_size}\n")
+            file.write(f"    Vaccination Rate: {vax_percentage}\n")
+            file.write(f"          Virus Name: {virus_name}\n" )
+            file.write(f"      Mortality Rate: {mortality_rate}\n")
+            file.write(f"      Infection Rate: {infection_rate}\n")
+            file.write(f" # initial infection: {initial_infected}\n")
+            file.write(f"   # of interactions: {interactions}\n")
+        file.close()
+
+        #Print same data to console:
+        print(f"     Population Size: {population_size}")
+        print("    Vaccination Rate: {vax_percentage}")
+        print("          Virus Name: {virus_name}" )
+        print("      Mortality Rate: {mortality_rate}")
+        print("      Infection Rate: {infection_rate}")
+        print(" # initial infection: {initial_infected}")
+        print("   # of interactions: {interactions}")
+
 
     def log_interaction(self, person1, person2, did_infect=None,
                         person2_vacc=None, person2_sick=None):
@@ -82,7 +106,47 @@ class Logger(object):
         # all the possible edge cases!
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+        """
+        Documentation:
+        - Expects person1 and person2 as person objects.
+        - Expects did_infect, person2_vacc, and person2_sick as Booleans, if passed.
+        - Between the values passed with did_infect, person2_vacc, and person2_sick, this method
+            should be able to determine exactly what happened in the interaction and create a String
+            saying so.
+        - The format of the log should be "{person1.ID} infects {person2.ID}", or, for other edge
+            cases, "{person1.ID} didn't infect {person2.ID} because {'vaccinated' or 'already sick'}"
+        - Appends the interaction to logfile.
+        """
+        """
+        Pseudocode:
+        # check if person1 is infected:
+            # Create log: {person1.ID} infects {person2.ID}
+        # check if person1 is not infected:
+            # Create log: {person1.ID} didn't infect {person2.ID}
+        # if person 2 does not have a virus and is not vaccinated
+            # then {person1.ID} didn't infect {person2.ID} because {'vaccinated'}
+        # check if the random person does not have a virus and is not vaccinated
+            # if the random person meets these requirements then we generate a
+            random number between 0 and 1
+            # check if that number is less than or equal to the basic
+            reproduction rate
+            # if the random number is less than the reproduction rate then we
+            add the id of that person to the newly infected array
+            # if the random number is greater than the reproduction rate then we
+            then do nothing
+        """
+        with open(self.file_name, "w") as file:
+            # if person1 has the virus then person1 infects person2
+            if person1.infected != None:
+                file.write(f"/n {person1.ID} infects {person2.ID} because already sick.")
+            # if person2 has the virus then person1 infects person2
+            if person2.infected != None:
+                file.write(f"/n {person2.ID} infects {person1.ID} because already sick.")
+            if person1.is_vaccinated = True:
+                file.write(f"/n {person1.ID} didn't infect {person2.ID} because vaccinated.")
+            if person2.is_vaccinated = True:
+                file.write(f"/n {person2.ID} didn't infect {person1.ID} because vaccinated.")
+        file.close()
 
     def log_infection_survival(self, person, did_die_from_infection):
         # TODO: Finish this method.  The Simulation object should use this method to log
@@ -92,7 +156,17 @@ class Logger(object):
         # on the format of the log.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+        """
+        Pseudocode:
+        if person.is_alive == True:
+            did_die_from_infection = false
+        else:
+            did_die_from_infection = True
+        """
+        if person.is_alive == True:
+            did_die_from_infection = False
+        else:
+            did_die_from_infection = True
 
     def log_time_step(self, time_step_number):
         # TODO: Finish this method.  This method should log when a time step ends, and a
